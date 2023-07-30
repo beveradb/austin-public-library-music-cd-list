dfe4958a9aad003df5488e9fde76c22caab8aa48
Andrew Beveridge <andrew.beveridge@transferwise.com>
2023-07-30 13:35:04 -0500

    First pass at actual working output

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import os
+import csv
+import json
 import requests
 from bs4 import BeautifulSoup
 
 CACHE_FOLDER = "cache"
+MAX_PAGES_TO_FETCH = 10  # Set the maximum number of pages to fetch in a single run
 
 
 def get_json_data(url):
@@ -44,12 +47,89 @@ def get_json_data(url):
     return json_data
 
 
-# Replace this URL with the actual URL you want to fetch
-url = "https://austin.bibliocommons.com/v2/search?custom_edit=false&query=formatcode%3A(MUSIC_CD)&searchType=bl&suppress=true&f_CIRC=CIRC&sort=newly_acquired"
+def write_metadata_to_csv(cd_metadata_list):
+    fields = ["Artist", "Title"]  # Add more fields as needed
+    csv_file_path = "cd_metadata.csv"
+    with open(csv_file_path, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        for cd_metadata in cd_metadata_list:
+            writer.writerow(cd_metadata)
 
-json_data = get_json_data(url)
 
-if json_data:
-    print("JSON data extracted successfully!")
-else:
-    print("Unable to fetch JSON data.")
+def parse_json_and_extract_metadata(json_data):
+    cd_metadata_list = []
+
+    try:
+        # Step 1: Parse the JSON data
+        data = json.loads(json_data)
+
+        # Step 2: Access the relevant section
+        bibs_data = data.get("entities", {}).get("bibs", {})
+
+        # Step 3: Iterate through each CD
+        for cd_id, cd_info in bibs_data.items():
+            try:
+                # Step 4: Extract artist and title
+                brief_info = cd_info.get("briefInfo", {})
+
+                # Default to Unknown Artist if the 'authors' list is empty
+                artist = "Unknown Artist"
+                artists = brief_info.get("authors")
+                if len(artists) > 0:
+                    artist = artists[0]
+
+                # Set 'title' to default if it's empty
+                title = brief_info.get("title", "Unknown Title") or "Unknown Title"
+
+                # Step 5: Store metadata in a list of dictionaries
+                cd_metadata = {
+                    "Artist": artist,
+                    "Title": title,
+                }
+                cd_metadata_list.append(cd_metadata)
+            except Exception as e:
+                print(f"Error processing CD with ID '{cd_id}':", e)
+
+    except json.JSONDecodeError as e:
+        print("Error parsing JSON:", e)
+    except Exception as e:
+        print("An error occurred while processing CD data:", e)
+
+    return cd_metadata_list
+
+
+def fetch_all_cd_metadata():
+    base_url = "https://austin.bibliocommons.com/v2/search"
+    query_params = {
+        "custom_edit": "false",
+        "query": "formatcode:(MUSIC_CD)",
+        "searchType": "bl",
+        "suppress": "true",
+        "f_CIRC": "CIRC",
+        "sort": "newly_acquired",
+    }
+
+    all_cd_metadata = []
+
+    for page in range(1, MAX_PAGES_TO_FETCH + 1):
+        query_params["page"] = page
+        url = f"{base_url}?{'&'.join(f'{k}={v}' for k, v in query_params.items())}"
+
+        json_data = get_json_data(url)
+
+        if json_data:
+            cd_metadata_list = parse_json_and_extract_metadata(json_data)
+
+            all_cd_metadata.extend(cd_metadata_list)
+        else:
+            print("Failed to fetch JSON search results.")
+
+    if all_cd_metadata:
+        write_metadata_to_csv(all_cd_metadata)
+        print("All CD metadata fetched and CSV file written.")
+    else:
+        print("Unable to fetch CD metadata.")
+
+
+fetch_all_cd_metadata()
